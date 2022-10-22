75fd97c13f4c51e469ced7ba1de7bc4ee9ddd778
guilhermeMgimenez <94083701+guilhermeMgimenez@users.noreply.github.com>
2022-10-21 22:19:33 -0300

    bootstrap

--- aula07/app.js
+++ aula07/app.js
@@ -1,66 +1,57 @@
-function fnPegarUmPokemon(url){
-
- 
-
+/* 
+OBJETIVO: Criar uma lista de pokemons
+- consulta em uma api
+- pegar um pokemon
+- montar um cartao de pokemon
+- colocar na tela o card
+*/
+function fnPegarPokemons(){
     $.ajax({
-
-        type: "Get",
-
-        url: url,
-
-       // data: "data",
-
+        type: "GET",
+        url: "https://pokeapi.co/api/v2/pokemon?limit=151",
+        //data: "data",
         dataType: "json",
-
         success: function (dados) {
-
-        let Pokemon = {
-
-            "nome": dados.name,
-
-            "id": dados.id,
-
-            "foto": dados.sprites.front_default,
-
-            "tipo": dados.types[0].type.name,
-
+            $(".lista-pokemons").html("")
+            let dadosPokemons = dados.results
+            for (let i = 0; i < dadosPokemons.length; i++) {
+                fnPegarUmPokemon(dadosPokemons[i].url)
+            }
+        },
+        beforeSend: function(){
+            $(".lista-pokemons").html("Aguarde, carregando os dados")
         }
-
-        fnMontarCartao(Pokemon)
-
-        }
-
     });
-
 }
 
+function fnPegarUmPokemon(url){
+    $.ajax({
+        type: "GET",
+        url: url,
+        dataType: "json",
+        success: function (dados) {
+            let Pokemon = {
+                "nome": dados.name,
+                "id" : dados.id,
+                "foto" : dados.sprites.front_default,
+                "tipo" : dados.types[0].type.name,
+                "info":"novo"
+            }
+            fnMontarCartao(Pokemon)
+        }
+    });
+}
 
-
-
-function fnMontarCartao(Pokemon) {
-
-
-
+function fnMontarCartao(Pokemon){
     let cartao
-
-    cartao = `<li>`
-
-    cartao += `<h3>${Pokemon.name}</h3>`
-
-    cartao += `<p> <img src="${Pokemon.foto}"></p>`
-
-    cartao += `<p> ${Pokemon.tipo}</p>`
-
+    cartao = `<li class="card ${Pokemon.tipo}">`
+    cartao += `<h3>${Pokemon.nome}</h3>` 
+    cartao += `<p><img src="${Pokemon.foto}"></p>` 
+    cartao += `<p>${Pokemon.tipo}</p>`
     cartao += `<h4>${Pokemon.id}</h4>`
-
-    cartao += `</li>`
-
-
-
+    cartao += `</li>` 
     $('.lista-pokemons').append(cartao)
-
 }
-
 
 
 fnPegarPokemons()
